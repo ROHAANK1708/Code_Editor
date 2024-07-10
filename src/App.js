437b0c9e368e5de0f34293eaa3501e57b18c6ffd
ROHAANK1708 <101565609+ROHAANK1708@users.noreply.github.com>
2024-07-10 20:05:01 +0530

    ho gaya

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { Toaster } from "react-hot-toast";
 import Home from "./pages/Home";
 import EditorPage from "./pages/EditorPage";
 import { RecoilRoot } from "recoil";
-import { io } from 'socket.io-client';
-io.connect("http://localhost:5000");
+
+
 
 
 function App() {
